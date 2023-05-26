3f2c132af2ed7a05a4a465600226114e85644044
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-05-26 10:44:33 +0545

    circuit breaker using resilience

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,3 @@
 
 M
 discoveryservice/build.gradle,6/2/6212e4e756ec86d179fe9344476b708fcaeb4f64
-„
-Tdiscoveryservice/src/main/java/com/discoveryservice/DiscoveryserviceApplication.java,1/5/15feef15bbda7e100ab6577af8a4bb9d03ae827c
--- userservice/src/main/java/com/userservice/controller/UserController.java
+++ userservice/src/main/java/com/userservice/controller/UserController.java
@@ -3,29 +3,46 @@ package com.userservice.controller;
 import com.userservice.dto.UserResponse;
 import com.userservice.model.User;
 import com.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public String saveUser(@RequestBody User user){
+    public String saveUser(@RequestBody User user) {
         userService.saveUser(user);
         return "saved successfully";
     }
 
     @GetMapping("/users/{id}")
-    public UserResponse getUserById(@PathVariable Long id){
-        return userService.getUserById(id);
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id),HttpStatus.OK);
+    }
+
+    public ResponseEntity<UserResponse> ratingHotelFallback(Long id, Exception exception) {
+        log.info("Fallback is executed because " + exception);
+        UserResponse userResponse = UserResponse.builder()
+                .name("dummy")
+                .about("Service is down")
+                .email("dummy@gmail.com")
+                .build();
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @GetMapping("/users")
-    public List<UserResponse> getAllUser(){
+    public List<UserResponse> getAllUser() {
         return userService.getAllUsers();
     }
 
